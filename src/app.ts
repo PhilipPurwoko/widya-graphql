a69d11a6acc581bf97d85ce4d37f0708f5e6d611
Philip Purwoko <philippurwoko123@gmail.com>
2021-07-09 16:43:32 +0700

    :tulip: Add index, 404, 500 page

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
-import { fastify, FastifyInstance } from 'fastify'
+import { fastify, FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { Server, IncomingMessage, ServerResponse } from 'http'
+import view from 'point-of-view'
+import ejs from 'ejs'
+import path from 'path'
 
-const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({ 'logger': true })
+const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify()
 
-app.get('/', (_req, res) => {
-    res.send('hello world')
+// Set View Engine
+app.register(view, {
+    root: path.join(__dirname, 'view'),
+    viewExt: 'ejs',
+    engine: {
+        ejs: ejs
+    }
 })
 
-app.listen(3000, (err, address) => {
+app.get('/', (_req: FastifyRequest, res: FastifyReply) => {
+    res.view('index')
+})
+
+app.setNotFoundHandler((_req: FastifyRequest, res: FastifyReply) => {
+    res.view('404')
+})
+
+app.setErrorHandler((err: FastifyError, _req: FastifyRequest, res: FastifyReply) => {
+    console.log(err)
+    res.view('500')
+})
+
+app.listen(3000, (err: Error, address: string) => {
     console.log(`Server is now listening on ${address}`)
     if (err) throw err
 })
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,3 +1,12 @@
-test('Dummy test', () => {
-    expect(0).toBe(0)
+import * as fs from 'fs'
+import * as path from 'path'
+
+test('View files are exist', () => {
+    fs.readdir(path.join(__dirname, '../src', 'view'), (_err: NodeJS.ErrnoException, files: string[]) => {
+        expect(files.sort()).toEqual([
+            'index.ejs',
+            '404.ejs',
+            '500.ejs'
+        ].sort())
+    })
 })
